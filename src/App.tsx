04fa581ab164976ef90f5745b1633618efc9dc65
Marwa Mahmoud <mm3764453@gmail.com>
2024-06-17 12:34:12 +0300

    add custom modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,51 @@
 /** @format */
 
+import { useState } from "react";
 import "./App.css";
 import ProductCard from "./components/ProductCard";
 import Modal from "./components/ui/Modal";
 import { productList } from "./data/data";
+import Button from "./components/ui/Button";
 
 function App() {
-  // Renders
+  /*___________ State___________*/
+  const [isOpen, setIsOpen] = useState(false);
+  /*___________ Handler___________*/
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  /*___________ Renders___________*/
   const renderProductsList = productList.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
   return (
-    <main className="grid grid-cols-1 gap-4 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 container  py-5 ">
-      {renderProductsList}
-      <Modal/>
+    <main className="container  py-5 ">
+      <Button bgClass="bg-red-700" onClick={openModal}>
+        open Modal
+      </Button>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 ">
+        {renderProductsList}
+        <Modal
+          isOpen={isOpen}
+          closeModal={closeModal}
+          title="Add A New Product"
+        >
+          <Button
+            bgClass="bg-indigo-700 hover:bg-indigo-800"
+            onClick={() => setIsOpen(false)}
+          >
+            Cancel
+          </Button>
+          <Button bgClass="bg-indigo-700" onClick={closeModal}>
+            Deactivate
+          </Button>
+        </Modal>
+      </div>
     </main>
   );
 }
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,37 +1,20 @@
 /** @format */
-import {
-  Description,
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-} from "@headlessui/react";
-import { useState } from "react";
-
-const Modal = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import type { ReactNode } from "react";
+interface IProps {
+  title?: string;
+  children: ReactNode;
+  isOpen: boolean;
+  closeModal: () => void;
+}
+const Modal = ({ title, closeModal, isOpen, children }: IProps) => {
   return (
     <>
-      <button onClick={() => setIsOpen(true)}>Open dialog</button>
-      <Dialog
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-        className="relative z-50"
-      >
+      <Dialog open={isOpen} onClose={closeModal} className="relative z-50">
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
           <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
-            <DialogTitle className="font-bold">Deactivate account</DialogTitle>
-            <Description>
-              This will permanently deactivate your account
-            </Description>
-            <p>
-              Are you sure you want to deactivate your account? All of your data
-              will be permanently removed.
-            </p>
-            <div className="flex gap-4">
-              <button onClick={() => setIsOpen(false)}>Cancel</button>
-              <button onClick={() => setIsOpen(false)}>Deactivate</button>
-            </div>
+            {title && <DialogTitle className="font-bold">{title}</DialogTitle>}
+            <div className="flex gap-4">{children}</div>
           </DialogPanel>
         </div>
       </Dialog>
